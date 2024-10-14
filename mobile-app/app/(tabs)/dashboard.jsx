82a363c3890d13563ba949647b65da15f8f18457
YourGothDaddy <alexanderorjustalex@gmail.com>
2024-10-14 12:45:41 +0300

    Redesign the sub-menu within dashboard

--- mobile-app/app/(tabs)/dashboard.jsx
+++ mobile-app/app/(tabs)/dashboard.jsx
@@ -1,18 +1,64 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Colors } from "@/constants/Colors";
 import GeneralView from "../components/tabviews/generalView";
 import ChartsView from "../components/tabviews/chartsView";
 
 const Dashboard = () => {
-  const [activeTab, setActiveTab] = useState("general");
+  const tabs = ["General", "Charts"];
+
+  const [activeTab, setActiveTab] = useState("General");
+  const translateX = useRef(new Animated.Value(0)).current;
+  const lineWidth = useRef(new Animated.Value(0)).current;
+  const tabLayouts = useRef([]).current;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [initialLoad, setInitialLoad] = useState(false);
+
+  const onTabPress = (index, tab) => {
+    setSelectedIndex(index);
+    animateLine(index);
+    setActiveTab(tab);
+  };
+
+  const animateLine = (index) => {
+    const { x, width } = tabLayouts[index];
+
+    Animated.parallel([
+      Animated.timing(translateX, {
+        toValue: x,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(lineWidth, {
+        toValue: width,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  const onTabLayout = (event, index) => {
+    const { x, width } = event.nativeEvent.layout;
+    tabLayouts[index] = { x, width };
+
+    if (!initialLoad) {
+      setInitialLoad(true);
+      animateLine(selectedIndex);
+    }
+  };
 
   const renderContent = () => {
     switch (activeTab) {
-      case "general":
+      case "General":
         return <GeneralView />;
-      case "charts":
+      case "Charts":
         return <ChartsView />;
       default:
         return <GeneralView />;
@@ -21,35 +67,41 @@ const Dashboard = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.contentWrapper}>
-        <View>
-          <Text style={styles.appTitle}>Fitlicious</Text>
-        </View>
-        <View>
-          <Text style={styles.pageTitle}>Charts</Text>
-        </View>
+      <View style={styles.navigationContainer}>
+        <Text className="text-4xl font-pextrabold text-center text-green pt-10">
+          Fitlicious
+        </Text>
+        <Text style={styles.pageTitle}>Dashboard</Text>
         <View style={styles.tabContainer}>
-          <TouchableOpacity
-            onPress={() => setActiveTab("general")}
-            style={[
-              styles.tabButton,
-              activeTab === "general" && styles.activeTabButton,
-            ]}
-          >
-            <Text style={styles.tabButtonText}>General</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setActiveTab("charts")}
-            style={[
-              styles.tabButton,
-              activeTab === "charts" && styles.activeTabButton,
-            ]}
-          >
-            <Text style={styles.tabButtonText}>Charts</Text>
-          </TouchableOpacity>
+          {tabs.map((tab, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => onTabPress(index, tab)}
+              onLayout={(event) => onTabLayout(event, index)}
+              style={styles.tabButton}
+            >
+              <Text
+                style={[
+                  styles.tabButtonText,
+                  activeTab === tab && styles.activeTabButtonText,
+                ]}
+              >
+                {tab}
+              </Text>
+            </TouchableOpacity>
+          ))}
         </View>
-        {renderContent()}
+        <Animated.View
+          style={[
+            styles.activeTabIndicator,
+            {
+              width: lineWidth,
+              transform: [{ translateX }],
+            },
+          ]}
+        />
       </View>
+      <View style={styles.contentWrapper}>{renderContent()}</View>
     </SafeAreaView>
   );
 };
@@ -59,15 +111,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.white.color,
   },
+  navigationContainer: {
+    backgroundColor: Colors.white.color,
+    paddingBottom: 15,
+    borderWidth: 2,
+    borderTopWidth: 0,
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5,
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 50,
+  },
   contentWrapper: {
     flex: 1,
-  },
-  appTitle: {
-    fontSize: 36,
-    fontWeight: "800",
-    textAlign: "center",
-    color: Colors.green.color,
-    paddingTop: 40,
   },
   pageTitle: {
     fontSize: 24,
@@ -78,19 +133,29 @@ const styles = StyleSheet.create({
   },
   tabContainer: {
     flexDirection: "row",
-    justifyContent: "space-around",
-    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    paddingBottom: 10,
   },
   tabButton: {
-    padding: 10,
-    backgroundColor: Colors.lightGreen.color,
-    borderRadius: 5,
-  },
-  activeTabButton: {
-    backgroundColor: Colors.green.color,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
   },
   tabButtonText: {
-    color: Colors.white.color,
+    color: Colors.green.color,
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  activeTabButtonText: {
+    color: Colors.green.color,
+    fontWeight: "700",
+  },
+  activeTabIndicator: {
+    position: "relative",
+    bottom: 0,
+    height: 2,
+    backgroundColor: Colors.green.color,
   },
 });
 
