8d218577402c42d416c9b629dff7f7e6e034cded
YourGothDaddy <alexanderorjustalex@gmail.com>
2025-07-15 18:41:49 +0300

    Change back-end logging level

--- Back-end/obj/Debug/net8.0/Fitness-Tracker.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Fitness-Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitness-Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae639ea4df6dbafd117bcaa9f4fa7cded833fb52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc70200f7024baea7c3f8a9effaff52816331378")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitness-Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitness-Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
